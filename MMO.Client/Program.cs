b8c96116b4dcdcf1a80669c52b26b905e93e1fbc
Seven <chase@chasehuegel.io>
2023-04-13 00:12:23 -0400

    client config

--- MMO.Client/Program.cs
+++ MMO.Client/Program.cs
@@ -2,18 +2,22 @@
 using MMO.Bridge.Packets;
 using MMO.Client;
 using MMO.Client.Commands;
+using MMO.Client.Models;
 using MMO.Client.Services;
 using Swordfish.Library.Networking;
+using Swordfish.Library.Util;
 
 PacketManager.RegisterAssembly<ChatPacket>();
 PacketManager.RegisterAssembly();
+
+ClientConfig config = Config.Load<ClientConfig>("config/client.toml");
 
 var application = new Application();
 
 var netClientSettings = new NetControllerSettings { KeepAlive = TimeSpan.FromSeconds(10) };
 var netClient = new NetClient(netClientSettings);
 
-var portalService = new PortalService("https://192.168.1.232:7297");
+var portalService = new PortalService(config.Connection.PortalUrl);
 
 var commandParser = new CommandParser(
     indicator: '\0',
